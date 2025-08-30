4ded09029d47a08266edc3cae2b19b885369d61f
Jesvin_SV <94107320+Jesvin-27@users.noreply.github.com>
2025-08-30 12:12:40 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAe4IAj45dyGnb9cTw6BQpYFru1fk1-mE4",
-  authDomain: "ai-rehab-349a4.firebaseapp.com",
-  projectId: "ai-rehab-349a4",
-  storageBucket: "ai-rehab-349a4.firebasestorage.app",
-  messagingSenderId: "909919340046",
-  appId: "1:909919340046:web:f2b1b370ca702e20aede5f",
-  measurementId: "G-8FVQGJ8LC3"
+  apiKey: 
+  authDomain: 
+  projectId: 
+  storageBucket: 
+  messagingSenderId: 
+  appId: 
+  measurementId: 
 };
 
 const app = initializeApp(firebaseConfig);
